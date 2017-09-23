2a4dc05c3925f1e02e1e55042f2eb2e4e4a12ee6
melheins <heins.mel@gmail.com>
2017-09-23 10:56:18 -0400

    WIP JS file

---
+++ app.js
@@ -0,0 +1,43 @@
+$(document).ready(function () {
+    // Initialize Firebase
+    var config = {
+        apiKey: "AIzaSyDIpq_qDmAW5koddC_e2CCB8JBULPgfc9k",
+        authDomain: "gtcbc7-2-1.firebaseapp.com",
+        databaseURL: "https://gtcbc7-2-1.firebaseio.com",
+        projectId: "gtcbc7-2-1",
+        storageBucket: "gtcbc7-2-1.appspot.com",
+        messagingSenderId: "473723028660"
+    };
+    firebase.initializeApp(config);
+
+    // Assign the reference to the database to a variable named 'database'
+    var database = firebase.database();
+
+    // Initialize Global Variables
+    var obj = {};
+
+
+    // on submit button do this
+    $("#").on("click", function () {
+
+        // Don't refresh the page!
+        event.preventDefault();
+
+        // Grab data input
+        var newobj = {};
+        newobj.name = $("#name-input").val().trim();
+        newobj.role = $("#role-input").val().trim();
+        newobj.startDate = $("#start-date-input").val().trim();
+        newobj.monthlyRate = $("#monthly-rate-input").val().trim();
+
+        // Send data to firebase
+        database.ref('/Activity17').set({
+            data: newobj
+        });
+        // Update screen with data
+
+
+    });
+
+})
+;
